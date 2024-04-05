fda8272b31b3abb457abbca1a2958d23c5d35b62
benbasil1 <benbasilreji6@gmail.com>
2024-04-06 00:27:12 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddRecipe from './components/AddRecipe';
+import Recipesearch from './components/Recipesearch';
 
 function App() {
   return (
     <div >
      <AddRecipe/>
+     <Recipesearch/>
     </div>
   );
 }
